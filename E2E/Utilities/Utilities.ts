b9a9c56a460deea314576e9e2d454fde2d26274f
omarbeto18 <57156491+omarbeto18@users.noreply.github.com>
2024-09-10 22:40:03 -0600

    Adding comments to the scripts

--- E2E/Utilities/Utilities.ts
+++ E2E/Utilities/Utilities.ts
@@ -1,5 +1,6 @@
 export default class Utilities{
 
+  //Utility to create a random number receiving a min and max value that is the range of numbers
     static getRandom(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
--- E2E/test-2.spec.ts
+++ E2E/test-2.spec.ts
@@ -6,7 +6,9 @@ import LoginModal from './Pages/LoginModal';
 import Checkout from './Pages/Checkout';
 import Payment from './Pages/Payment';
 
+//Create the test to be implemented and executed
 test('test', async ({ page }) => {
+  //Create instances of all the objects of each of the components of the POM
   const product = new Products(page);
   const home = new Home(page);
   const cart = new Cart(page);
@@ -14,29 +16,43 @@ test('test', async ({ page }) => {
   const checkout = new Checkout(page);
   const payment = new Payment(page);
 
+//Go to the main page and open it
   await page.goto('https://automationexercise.com/');
+  
+//Go to the product section  
   const locator = page.getByText(' Products');
   await locator.click();
   await page.pause();
-  
+
+//Open the product description of the item selected
   await product.gotoProducts();
+
+//Add products with a random quantity to the cart
   await product.addProductToCart();
 
+//Open the cart
   await home.gotoCart();
+
+//Proceed to the checkout page  
   await cart.proceedtoCheckout();
  
+//Login on the login modal  
   await loginmodal.Login();
  
-  await home.gotoCart();
- 
+//Go to the cart and proceed to checkout
+  await home.gotoCart(); 
   await cart.proceedtoCheckout();
 
+//Place the order  
   await checkout.placeOrder();
+
+//Send payment information and Confirm the Order
 
   await payment.sendPaymentInformation();
 
   await payment.confirmOrder();
 
+//Log out from your session
   await home.logout();
  
 });
